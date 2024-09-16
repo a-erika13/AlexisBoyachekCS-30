e249d5d5a4ea39e1c45cd027f383f080482eddcd
Alexis <alexis.boyachek@rcsd.ca>
2024-09-16 08:54:17 -0600

    Friday Edits

---
+++ 1-Lesson Demos/Lesson 4 Demo/src/App.java
@@ -0,0 +1,10 @@
+// Alexis Boyachek
+// Lesson 4 Demo
+// Computer Science 30
+// 13 September 2024
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
