de5834970f00676cfc09b6efba12f6a7b5c015da
Alexis <alexis.boyachek@rcsd.ca>
2024-09-16 09:49:22 -0600

    ASCII Art AEB

--- A-4 ASCII ART/src/App.java
+++ A-4 ASCII ART/src/App.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+//Name: Alexis Boyachek
+//Date: 16 September 2024
+//Class: Computer Science 30 Period 1
+//Project: ASCII Art AEB
+public class App 
+{
+    public static void main ( String[] args )
+    {
+        System.out.println("Alexis Boyachek\\ 16 September 2024 \\ Period 1\\");
+        System.out.println("What will you draw?");
+        System.out.println("A plant");
+
+        System.out.println("            __/)");
+        System.out.println("         .-(__(=:");
+        System.out.println("         |     \\)");
+        System.out.println("         |");
+        System.out.println("   (\\__  |");
+        System.out.println("  :=)__)-|  __/)");
+        System.out.println("   (/    |-(__(=:");
+        System.out.println("  _____  |   _ \\)");
+        System.out.println(" /     \\ |  / \\");
+        System.out.println("     ___\\| /__\\");
+        System.out.println("  [            ]\\");
+        System.out.println("   \\          / \\");
+        System.out.println("    \\        /");
+        System.out.println("     \\______/");
     }
 }
+
