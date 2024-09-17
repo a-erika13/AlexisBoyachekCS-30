002d5809c75336dc1c87f3ed5909178811989c2e
Alexis <alexis.boyachek@rcsd.ca>
2024-09-17 09:15:43 -0600

    A-4 ASCII Art Edits
    09/17/2024

--- A-4 ASCII ART/src/App.java
+++ A-4 ASCII ART/src/App.java
@@ -1,14 +1,15 @@
 //Name: Alexis Boyachek
-//Date: 16 September 2024
+//Date: 17 September 2024
 //Class: Computer Science 30 Period 1
-//Project: ASCII Art AEB
+//Project: A-4 Flower ASCII Art AEB
 public class App 
 {
     public static void main ( String[] args )
     {
+        //Project Description: ASCII Art- drawing using symbol in string codes (ex. /, _, \, etc.)
         System.out.println("Alexis Boyachek\\ 16 September 2024 \\ Period 1\\");
         System.out.println("What will you draw?");
-        System.out.println("A plant");
+        System.out.println("A flower");
 
         System.out.println("            __/)");
         System.out.println("         .-(__(=:");
@@ -19,11 +20,17 @@ public class App
         System.out.println("   (/    |-(__(=:");
         System.out.println("  _____  |   _ \\)");
         System.out.println(" /     \\ |  / \\");
-        System.out.println("     ___\\| /__\\");
+        System.out.println("   _____\\|_/___\\");
         System.out.println("  [            ]\\");
-        System.out.println("   \\          / \\");
+        System.out.println("   \\          /  \\");
         System.out.println("    \\        /");
         System.out.println("     \\______/");
+        System.out.println("||||||| ||");
+        System.out.println("||      ||   |||||   ||         ||  |||||   ||  |||");
+        System.out.println("||||    ||  ||   ||   ||       ||  ||___||  ||||   ||");
+        System.out.println("||      ||  ||   ||    || ||| ||    ||      ||");
+        System.out.println("||      ||   |||||     |||| ||||     |||||  ||");
+
     }
 }
 
