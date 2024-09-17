d83aa50cdf723e5d3921b7fda6f9161edaabe0c5
Alexis <alexis.boyachek@rcsd.ca>
2024-09-17 09:49:13 -0600

    09/17/2024

---
+++ 1-Lesson Demos/LessonVariablesDemo/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(1/3);
+        System.out.println(1.0/3);
+        System.out.println(1/3.0);
+        System.out.println(1.0/3.0);
+        System.out.println((double) 2/3);
+        int myNum = 2147483647;
+        System.out.println(myNum);
+        int num = Math.random();
+        System.out.println(num);
+    }
+}
