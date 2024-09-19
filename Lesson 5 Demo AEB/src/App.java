8cecb33dfaf0434171690a919e4ddcaab09d6514
Alexis <alexis.boyachek@rcsd.ca>
2024-09-19 09:48:22 -0600

    end of class demo 09/19/2024

---
+++ Lesson 5 Demo AEB/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 ="This is a test";
+        String message2 = "Hello class";
+        System.out.println(message1.length());
+        System.out.println(message2.length());
+
+        System.out.println(message1.substring(0,3));
+        System.out.println(message1.substring(5));
+    }
+}
