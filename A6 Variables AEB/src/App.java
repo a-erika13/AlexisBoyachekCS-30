376dab1afa8121136ed4e9b51948c4696028842a
Alexis <alexis.boyachek@rcsd.ca>
2024-09-20 09:34:43 -0600

    end of class friday

--- A6 Variables AEB/src/App.java
+++ A6 Variables AEB/src/App.java
@@ -1,3 +1,8 @@
+//Name: Alexis Boyachek
+//Date: 20 September 2024
+//Class: Computer Science 30 Period 1
+//Project: A-6 Varibles AEB
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
