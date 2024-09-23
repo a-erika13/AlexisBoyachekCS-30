31726fbf1830d2147f6ca56ca15c7e31b4bf5e2b
Alexis <alexis.boyachek@rcsd.ca>
2024-09-23 09:43:08 -0600

    09/23/2024

--- Assignments/A-7 Strings AEB/src/App.java
+++ Assignments/A-7 Strings AEB/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String message1 = "I am very happy";
+        String target1 = "very";
+        System.out.println();
+
+        String message2 = "That was great -- lol";
+        String target2 = "lol";
+
+        \\Your code for part one should remove the word “very ” (and following space) from message1 and print the new message.
+
+        \\You must use .indexOf() to find the start position of a the part of the string you want to remove.
+
+        \\You need to use .length() to find the end point of the string you want to remove.
+
+        \\You can then use .substring() to create a new string removing the word
     }
 }
---
+++ Assignments/Lesson 5 Input/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("what is your name?");
+        String name = keys .next();
+        System.out.println("You entered: " + name);
+    
+    }
+}
