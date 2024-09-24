6bb18a238e9f913559ee4eb625de3555c31c68f7
Alexis <alexis.boyachek@rcsd.ca>
2024-09-24 09:38:24 -0600

    A-6 09/24/2024

--- Assignments/A-6 Variables AEB/src/App.java
+++ Assignments/A-6 Variables AEB/src/App.java
@@ -1,15 +1,17 @@
 //Name: Alexis Boyachek
-//Date: 20 September 2024
+//Date: 24 September 2024
 //Class: Computer Science 30 Period 1
 //Project: A-6 Varibles AEB
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // define variables (Price, availale money, wings you can purchase, and how much it will cost)
         double wingPrice = 0.75;
         double money = 4.75;
         double buyWings = (int)(money / wingPrice);
         double wingTotal = (wingPrice * buyWings);
 
+        // tell user how many wings they can buy and how much it will cost
         System.out.println("With $4.75, you can purchase: " + buyWings);
         System.out.println("for the price of: $" + wingTotal);
     }
