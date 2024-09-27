8361eb3fbeb774d8b3b91cd7b52a4e190602dc49
Alexis <alexis.boyachek@rcsd.ca>
2024-09-27 09:39:40 -0600

    Runner Class Demo 09/27/2024

---
+++ .Lesson Demos/Lesson 6 Demo/src/TurkeyRunner.java
@@ -0,0 +1,8 @@
+public class TurkeyRunner {
+    public static void main(String[] args) throws Exception {
+        Turkey bird = new Turkey();
+
+        bird.speak();
+        bird.speak();
+    }
+}
