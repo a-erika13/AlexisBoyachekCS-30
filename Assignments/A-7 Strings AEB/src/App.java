9e761539929811219e36878dd8916331a61deb47
Alexis <alexis.boyachek@rcsd.ca>
2024-10-01 09:32:00 -0600

    A-7

--- Assignments/A-7 Strings AEB/src/App.java
+++ Assignments/A-7 Strings AEB/src/App.java
@@ -1,8 +1,15 @@
 public class App {
+//Name: Alexis Boyachek
+//Date: 17 September 2024
+//Class: Computer Science 30 Period 1
+//Project: A-7 Strings AEB
+
     public static void main(String[] args) throws Exception {
         String message1 = "I am very happy";
         System.out.println(message1);
+        // use indexOf() to find out where the word you are wanting to remove begins
         System.out.println(message1.indexOf("very"));
+        // use length() to find out whwere the collective string ends
         System.out.println(message1.length());
         System.out.println(message1.substring(0,5) + message1.substring(10,15));
         
@@ -10,7 +17,9 @@ public class App {
 
         String message2 = "That was great -- lol";
         System.out.println(message2);
+        // use indexOf() to find out where the word you are wanting to remove begins
         System.out.println(message2.indexOf("lol"));
+        // use length() to find out whwere the collective string ends
         System.out.println(message2.length());
         System.out.println(message2.substring(0,18) + "Laugh Out Loud");
     }
