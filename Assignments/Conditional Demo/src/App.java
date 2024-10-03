5b8d3f5e9a32b147ada4cd7f239caa8297dd727e
Alexis <alexis.boyachek@rcsd.ca>
2024-10-03 08:54:16 -0600

    10/03/2024

---
+++ Assignments/Conditional Demo/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean isCold = false;
+        
+        if (isCold=true); {
+            System.out.println("Wear a coat");
+            System.out.println("Wear gloves");
+        }
+        System.out.println("Bye");
+    }
+}
