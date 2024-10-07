a88af44ddcc712118700875cfc448c1982857025
Alexis <alexis.boyachek@rcsd.ca>
2024-10-07 00:22:23 -0600

    A-8b (have not run yet)

---
+++ Assignments/A-8b AEB/src/Autumn.java
@@ -0,0 +1,17 @@
+public class Autumn {
+    //Name: Alexis Boyachek
+    //Date: 5 October 2024
+    //Class: Computer Science 30 Period 1
+    //Project: A-8b Input AEB
+    //Description: Sub-class that holds two methods, that will be called by the runner and display the outputs 
+    import random()
+    public static void main(String[] args) throws Exception {
+        public void listIntro(){
+            System.out.println("Autumn/Fall is here! This season typically lasts from September to November.")
+            System.out.println("Here is a list of thing associated with Fall:")
+        }
+        public void autumnList(){
+            System.out.println("Thanksgiving, Gilmore Girls, pumpkin pie, Halloween, leaves, vanilla");
+        }
+    }
+}
---
+++ Assignments/A-8b AEB/src/AutumnRunner.java
@@ -0,0 +1,13 @@
+public class AutumnRunner {
+    //Name: Alexis Boyachek
+    //Date: 5 October 2024
+    //Class: Computer Science 30 Period 1
+    //Project: A-8b Input AEB (Runner)
+    //Description: Runner for A-8b Project, which excecutes any/ all code for the sub-class Autumn.Java
+    public static void main(String[] args) throws Exception {
+        Autumn fall = new Autumn();
+
+        fall.listIntro();
+        fall.autumnList();
+    }
+}
