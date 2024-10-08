9e6fd35645b43c6226a72b5d9fba9602a15e25ed
Alexis <alexis.boyachek@rcsd.ca>
2024-10-07 19:27:20 -0600

    10/07/2024
    attempt to fix Java

--- Assignments/A-8b AEB/src/Autumn.java
+++ Assignments/A-8b AEB/src/Autumn.java
@@ -4,14 +4,13 @@ public class Autumn {
     //Class: Computer Science 30 Period 1
     //Project: A-8b Input AEB
     //Description: Sub-class that holds two methods, that will be called by the runner and display the outputs 
-    import random()
     public static void main(String[] args) throws Exception {
-        public void listIntro(){
-            System.out.println("Autumn/Fall is here! This season typically lasts from September to November.")
-            System.out.println("Here is a list of thing associated with Fall:")
-        }
-        public void autumnList(){
-            System.out.println("Thanksgiving, Gilmore Girls, pumpkin pie, Halloween, leaves, vanilla");
-        }
+    public void listIntro() {
+        System.out.println("Autumn/Fall is here! This season typically lasts from September to November.");
+        System.out.println("Here is a list of thing associated with Fall:");
+    }
+
+    public void autumnList() {
+        System.out.println("Thanksgiving, Gilmore Girls, pumpkin pie, Halloween, leaves, vanilla");
     }
 }
