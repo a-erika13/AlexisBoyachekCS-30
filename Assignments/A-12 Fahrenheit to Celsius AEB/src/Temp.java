3f1b7029806623c146292eed173cd59b6418df85
Alexis <alexis.boyachek@rcsd.ca>
2024-10-11 09:50:41 -0600

    10/11/2024

---
+++ Assignments/A-12 Fahrenheit to Celsius AEB/src/Temp.java
@@ -0,0 +1,9 @@
+public class Temp {
+    public void unitTemp(){
+        System.out.println("What unit will you be starting with: 1 for C, or 2 for F");
+        return;
+    }
+    public void tempValue(){
+        if unitTemp() = 1;
+    }
+}
---
+++ Assignments/A-12 Fahrenheit to Celsius AEB/src/TempRunner.java
@@ -0,0 +1,6 @@
+public class TempRunner {
+    public static void main(String[] args) throws Exception {
+        Temp FC = new Temp();
+        FC.unitTemp();
+    }
+}
