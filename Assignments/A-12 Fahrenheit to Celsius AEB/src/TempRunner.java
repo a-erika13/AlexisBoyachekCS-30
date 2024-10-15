865b99b6365e27b10427b6fbedd75a857538111a
Alexis <alexis.boyachek@rcsd.ca>
2024-10-15 09:48:21 -0600

    10/15/2024

--- Assignments/A-12 Fahrenheit to Celsius AEB/src/TempRunner.java
+++ Assignments/A-12 Fahrenheit to Celsius AEB/src/TempRunner.java
@@ -1,6 +1,16 @@
 public class TempRunner {
+        
+
+//Name: Alexis Boyachek
+//Date: 14 October 2024
+//Class: Computer Science 30 Period 1
+//Project: A-12 Fahrenheit to Celsius AEB
+//Description: Runner for methods to excecute code for unit tempurature converter (fahrenheit to celcius)
+
+//import scanner
     public static void main(String[] args) throws Exception {
-        Temp FC = new Temp();
-        FC.unitTemp();
+        public class TempRunner {
+            
+        }
     }
 }
