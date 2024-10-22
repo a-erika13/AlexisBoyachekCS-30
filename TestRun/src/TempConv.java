ebad6f7e7be0dd6f0fb79e38e19a218adc63768c
Alexis <alexis.boyachek@rcsd.ca>
2024-10-22 13:01:30 -0600

    10/22/2024

--- TestRun/src/TempConv.java
+++ TestRun/src/TempConv.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-
 public class TempConv {
+
     private Scanner f = new Scanner(System.in);
-    private double tempF;
+    private double tempF = f.nextDouble();
     private double C;
 
     public void insertF(){
         System.out.println("Enter temperature in fahrenheit: ");
-        tempF = f.nextDouble();
-
+        //tempF = f.nextDouble();
+        tempF = 32;
     }
 
     public void convertC(){
-        C = (5.0/9.0) * (tempF - 32);
+        C = (5.0/9.0) * tempF - 32;
 
     }
 
     public void result() {
         System.out.println("Your temperature conversion is " + C + "C");
     }
+
 }
