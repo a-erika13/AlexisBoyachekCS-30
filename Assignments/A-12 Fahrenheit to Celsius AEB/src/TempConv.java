248d9da9b3c9b78cbb7b102f3e9fe29f1d393918
Alexis <alexis.boyachek@rcsd.ca>
2024-10-22 12:48:42 -0600

    temp update

---
+++ Assignments/A-12 Fahrenheit to Celsius AEB/src/TempConv.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TempConv {
+    private Scanner f = new Scanner(System.in);
+    private double tempF = f.nextDouble();
+    private double C;
+
+    public void insertF(){
+        System.out.println("Enter temperature in fahrenheit: ");
+        tempF = f.nextDouble();
+
+    }
+
+    public void convertC(){
+        C = (5.0/9.0) * tempF - 32;
+
+    }
+
+    public void result() {
+        System.out.println("Your temperature conversion is " + C + "C");
+    }
+}
--- Assignments/A-12 Fahrenheit to Celsius AEB/src/TempRunner.java
+++ Assignments/A-12 Fahrenheit to Celsius AEB/src/TempRunner.java
@@ -1,17 +1,17 @@
-public class TempRunner {
-        
-
 //Name: Alexis Boyachek
 //Date: 14 October 2024
 //Class: Computer Science 30 Period 1
 //Project: A-12 Fahrenheit to Celsius AEB
 //Description: Runner for methods to excecute code for unit tempurature converter (fahrenheit to celcius)
-
 //import scanner
+
+public class TempRunner {
+        
     public static void main(String[] args) throws Exception {
-        TempRunner temp = new TempRunner();
+        TempConv temp = new TempConv();
         temp.insertF();
         temp.convertC();
+        temp.result();
 
     }
 }
