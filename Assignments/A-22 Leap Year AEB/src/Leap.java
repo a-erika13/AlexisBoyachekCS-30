8c2aad8427464494910021aeb84cd2f0eb2bdd7e
Alexis <alexis.boyachek@rcsd.ca>
2024-10-31 09:46:57 -0600

    10/31/2024

--- Assignments/A-22 Leap Year AEB/src/Leap.java
+++ Assignments/A-22 Leap Year AEB/src/Leap.java
@@ -1,5 +1,33 @@
+//Name: Alexis Boyachek
+//Date: 31 October 2024
+//Class: Computer Science 30 Period 1
+//Project: A-22 Leap Year AEB
+//Description: Prompts user to enter a year and the program will then output if the year was/ is/ will be a leap year
+import java.util.Scanner;
+
+//import scanner
 public class Leap {
+    private Scanner y = new Scanner(System.in);
+    private int leapYear;
+//create private constant variable so that user input can be called multiple times
+
+//create function that prompts user to enter a year
     public void insertYear(){
-        
+        System.out.println("Enter year: ");
+        leapYear = y.nextInt();
+    }
+//create function that outputs leap year or not with the perameters: IS divisible by 4, IS NOT divisible by 100, IS divisible by 400
+    public void calcLeap(){
+        if (leapYear % 4 == 0){
+            System.out.println(leapYear + " is a leap year");
+        }
+        else if (leapYear %100 != 0){
+            System.out.println(leapYear + " is not a leap year");
+        }
+        else if (leapYear %400 == 0){
+            System.out.println(leapYear + " is a leap year");
+        }
+    //print results
     }
 }
+
--- Assignments/A-22 Leap Year AEB/src/LeapRunner.java
+++ Assignments/A-22 Leap Year AEB/src/LeapRunner.java
@@ -1,5 +1,13 @@
+//Name: Alexis Boyachek
+//Date: 31 October 2024
+//Class: Computer Science 30 Period 1
+//Project: A-22 Miles Per Hour AEB
+//Description: leap year  program runner
 public class LeapRunner {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Leap ly = new Leap();
+//call functions from the other class
+        ly.insertYear();
+        ly.calcLeap();
     }
 }
