bf715a4a8b45a53fcdae57dd1a257a7c8bffc372
Alexis <alexis.boyachek@rcsd.ca>
2024-11-26 09:38:46 -0600

    11/26/2024

--- Assignments/A-23 Cool Numbers AEB/src/CoolNum.java
+++ Assignments/A-23 Cool Numbers AEB/src/CoolNum.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class CoolNum {
-    
+    private Scanner n = new Scanner(System.in);
+    private double cn;
+
+    public void enterNum() {
+        System.out.println("Enter a number: ");
+        cn = n.nextDouble();}
+
+    public void calcNum(){
+        if ((cn%3==1)  &&  (cn%4==1)  && (cn%5 ==1) && (cn%6 == 1)){
+            System.out.println("true");
+        }     
+    }  
 }
+
--- Assignments/A-23 Cool Numbers AEB/src/CoolNumRunner.java
+++ Assignments/A-23 Cool Numbers AEB/src/CoolNumRunner.java
@@ -1,5 +1,7 @@
 public class CoolNumRunner {
     public static void main(String[] args) throws Exception {
-    
+    CoolNum n = new CoolNum();
+    n.enterNum();
+    n.calcNum();
     }
 }
