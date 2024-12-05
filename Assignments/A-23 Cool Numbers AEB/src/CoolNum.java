911f1ab594773fb3983c8f88e501a05e0491f6c2
Alexis <alexis.boyachek@rcsd.ca>
2024-12-05 09:48:30 -0600

    12/05/2024

--- Assignments/A-23 Cool Numbers AEB/src/CoolNum.java
+++ Assignments/A-23 Cool Numbers AEB/src/CoolNum.java
@@ -9,18 +9,24 @@ public class CoolNum {
         System.out.println("Enter an integer: ");
         cn = n.nextInt();
     }
+
     public void isCoolNumber() {
-        //int x; 
-       // x = 6;
+       int x = 6;
+       int count = 0;
 
         if ((cn%3==1)  &&  (cn%4==1)  && (cn%5==1) && (cn%6==1)){
            System.out.println(cn + " is a cool number");
         }
-       // while (x) {
-            //if ((cn%3==1)  &&  (cn%4==1)  && (cn%5 ==1) && (cn%6 == 1)){}
-            //System.out.println("there are " + x + " cool numbers between 6 and " + cn);
+        
+       while (x <= cn) {
+            if  ((x%3==1)  &&  (x%4==1) && (x%5 ==1) && (x%6 == 1)){
+                count++;
+                x++;
+            }
         }
+        System.out.println("there are " + count + " cool numbers between 6 and " + cn);
 
     }
+}
 
 
---
+++ Assignments/A-31 Array Fun House AEB/src/ArrayFun.java
@@ -0,0 +1,4 @@
+public class ArrayFun {
+    int[] one = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
+    System.out.println(array[3] + array[6]);
+}
