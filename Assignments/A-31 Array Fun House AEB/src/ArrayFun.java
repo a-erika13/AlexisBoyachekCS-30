5d8523f6ab2f9b712537f310e6131bc1d6927711
Alexis <alexis.boyachek@rcsd.ca>
2024-12-05 09:48:52 -0600

    12/05

--- Assignments/A-31 Array Fun House AEB/src/ArrayFun.java
+++ Assignments/A-31 Array Fun House AEB/src/ArrayFun.java
@@ -1,4 +1,10 @@
+import java.util.Arrays;
+
 public class ArrayFun {
+    public void arrayOne(){
     int[] one = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
-    System.out.println(array[3] + array[6]);
+    for (int n=0; n < array.length; n++);
+	System.out.println(Arrays.toString(one));
+    System.out.println(getSum(one,3,6));
+    }
 }
