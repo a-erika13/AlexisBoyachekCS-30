f3ec767b0202c94ceed1be854b20e2c26286d8f8
Alexis <alexis.boyachek@rcsd.ca>
2025-01-08 09:34:26 -0600

    01/08/25

--- Assignments/A-24 Loop Stats AEB/src/LoopStats.java
+++ Assignments/A-24 Loop Stats AEB/src/LoopStats.java
@@ -9,9 +9,29 @@ public class LoopStats
 	private int start, stop;
 
 	public LoopStats()
-	{
+	{ while(n < 10){
+		//randomize numbers 1-100 using Math.random()
+		randInt1 = (int) (( Math.random() * 100 ) + 1);
+		randInt2 = (int) (( Math.random() * 100 ) + 1);
+		//print values
+		System.out.println("Your two random numbers are " + randInt1 + " and " + randInt2);
+		//create conditional statements to check which value is smaller/ larger
+		if(randInt1 < randInt2){
+			System.out.println("the smaller value is " + randInt1);
+			System.out.println("the larger value is " + randInt2);
+		}
+		else if(randInt1 > randInt2){
+			System.out.println("the larger value is " + randInt1);
+			System.out.println("the smaller value is " + randInt2);
+		}
+		else if(randInt1 == randInt2){
+			System.out.println("the two values are equal");
+		}
+		n = n + 1;
 	}
-
+if (totalN % 2 == 0){
+	
+}
 	public LoopStats(int beg, int end)
 	{
 	}
